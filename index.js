83287546b695c6becacb6d356e50880f5ab3270c
Katherine Wu <katherine.tsai.wu@gmail.com>
2018-08-02 23:53:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
